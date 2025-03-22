686e0a5160d69d50cc348f78896251bcbbe50700
Drstrange373 <105195165+Drstrange373@users.noreply.github.com>
2025-03-22 13:30:12 +0530

    initial commit

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import {
+    clerkMiddleware,
+    ClerkMiddlewareAuth,
+    createRouteMatcher,
+} from "@clerk/nextjs/server";
+import { NextRequest } from "next/server";
+
+const isPublicRoute = createRouteMatcher([
+    "/sign-in(.*)",
+    "/sign-up(.*)",
+    "/api/webhooks(.*)",
+]);
+
+export default clerkMiddleware(
+    async (auth: ClerkMiddlewareAuth, request: NextRequest) => {
+        if (!isPublicRoute(request)) {
+            await auth.protect();
+        }
+    },
+);
+
+export const config = {
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+        // Always run for API routes
+        "/(api|trpc)(.*)",
+    ],
+};
